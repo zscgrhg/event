d261a97593ee86dd08ef26aa76720fd9ee37fce1
zscgrhg <zscgrhg@qq.com>
2017-12-16 13:54:26 +0800

    666

--- src/main/java/com/example/event/beans/ListenerRegister.java
+++ src/main/java/com/example/event/beans/ListenerRegister.java
@@ -2,18 +2,21 @@ package com.example.event.beans;
 
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ListenerRegister {
     @EventListener
+    @Order(3)
     public void processUserEvent(UserEvent event) throws InterruptedException {
-        Thread.sleep(1000);
+        Thread.sleep(10000);
         System.out.println("            on event->" + event.getSource() + "  @ " + Thread.currentThread().getName());
     }
 
     @EventListener
+    @Order(4)
     @Async
     public void processUserEvent(ApplicationEvent event) throws InterruptedException {
         Thread.sleep(1000);
